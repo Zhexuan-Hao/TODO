e62dfed345344c95164817007271dd01d86b611a
LY <1287432999@qq.com>
2023-05-12 23:17:34 +0800

    graph for test

--- app/src/main/java/com/example/todo/ui/mine/MineFragment.java
+++ app/src/main/java/com/example/todo/ui/mine/MineFragment.java
@@ -53,9 +53,6 @@ public class MineFragment extends Fragment {
     private FragmentMineBinding binding;
 
 
-    public int Finished;
-    public int Unfinished;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SlideshowViewModel slideshowViewModel =
@@ -74,76 +71,6 @@ public class MineFragment extends Fragment {
 
         Statistic = binding.MineStatisticBtn;
         Logout = binding.MineLogoutBtn;
-
-//        Logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FirebaseAuth.getInstance().signOut();
-//                RemoveToken();
-//                Intent intent =new Intent(getActivity(), WelcomeActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//        // 获取 Firebase 实例
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        // 获取根引用
-//        DatabaseReference rootRef = database.getReference();
-//        // 在根引用上添加路径，获取指向特定节点的引用
-//        DatabaseReference usersRef = rootRef.child("Event");
-//        usersRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                Finished = 0;
-//                Unfinished = 0;
-//                for(DataSnapshot data : snapshot.getChildren()){
-//                    // 获取最新的数据
-//                    String tag = data.child("status").getValue().toString();
-//                    if(tag.equals("Finished!")){
-//                        Finished++;
-//                        System.out.println(tag);
-//                    } else if(tag.equals("Unfinished!")){
-//                        Unfinished++;
-//                    }
-//                }
-//
-//                List<PieEntry> pieEntryList=new ArrayList<>();
-//                pieEntryList.add(new PieEntry(Finished,"Finished!"));
-//                pieEntryList.add(new PieEntry(Unfinished,"Unfinished!"));
-//                PieDataSet pieDataSet=new PieDataSet(pieEntryList,"");
-//                //设置饼图颜色
-//                pieDataSet.setColors(Color.RED,Color.BLUE);
-//                //设置数据文本颜色
-//                pieDataSet.setValueTextColor(Color.WHITE);
-//                //设置数据文本大小
-//                pieDataSet.setValueTextSize(20);
-//                PieData pieData = new PieData(pieDataSet);
-//                //设置数据百分比显示
-//                pieData.setValueFormatter(new PercentFormatter());
-//                binding.pieChart.setData(pieData);
-//                //设置中心圆大小
-//                binding.pieChart.setHoleRadius(0);
-//                //设置中心圆颜色大小
-//                binding.pieChart.setTransparentCircleRadius(0);
-//                //右下角英文是否显示
-//                binding.pieChart.getDescription().setEnabled(false);
-//                //图例大小
-//                binding.pieChart.getLegend().setFormSize(10);
-//                //图例文本大小
-//                binding.pieChart.getLegend().setTextSize(10);
-//                //数据描述文本颜色
-//                binding.pieChart.setEntryLabelColor(Color.WHITE);
-//                //数据描述文本大小
-//                binding.pieChart.setEntryLabelTextSize(20);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                // 当监听器被取消时，执行该方法
-//            }
-//        });
 
         return root;
     }
