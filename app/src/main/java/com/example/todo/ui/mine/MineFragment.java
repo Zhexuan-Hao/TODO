7397251ba10f826499c4e55646a9fd2e312174f6
Minipax <925305364@qq.com>
2023-05-14 19:55:59 +0800

    5.14 用户信息修改1

--- app/src/main/java/com/example/todo/ui/mine/MineFragment.java
+++ app/src/main/java/com/example/todo/ui/mine/MineFragment.java
@@ -1,6 +1,8 @@
 package com.example.todo.ui.mine;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -34,6 +37,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -78,6 +82,61 @@ public class MineFragment extends Fragment {
             @Override
             public void onChanged(List<Event> events) {
                 localEvents = events;
+            }
+        });
+
+        binding.MineNicknameChangeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+
+                // 获取LayoutInflater对象，并使用inflate方法从XML布局文件中创建视图
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_edit_nickname, null);
+
+                // 获取对话框中的EditText并设置其文字为当前用户名
+                final EditText editText = dialogView.findViewById(R.id.nicknameEditText);
+                editText.setText(user.getDisplayName());
+
+                // 设置对话框中的视图
+                alertDialogBuilder.setView(dialogView);
+
+                // 设置PositiveButton点击事件
+                alertDialogBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 更新用户名EditText中的文字
+                        Nickname.setText(editText.getText().toString().trim());
+
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(editText.getText().toString().trim())
+                                .build();
+
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(getContext(), "Username updated!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+//                        Toast.makeText(getContext(), "Username updated!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // 设置NegativeButton点击事件
+                alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 取消对话框
+                        dialog.cancel();
+                    }
+                });
+
+                // 创建AlertDialog对象并显示
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
         });
 
